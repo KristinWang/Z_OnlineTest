c7c2290231ba932050d611e8924cff8d1f3e9122
Kristin Wang <ttwang@voxelcloud.net.cn>
2021-04-25 18:46:48 +0800

    change file name

--- src/constant/EnumTypes.js
+++ src/constant/EnumTypes.js
@@ -1,7 +1,7 @@
 
 export const dateFormatPreciseAtDay = 'YYYY-MM-DD';
 export const dateFormatPreciseAtMins = 'YYYY-MM-DD, HH:mm';
-
+// update
 export const testSectionMap = {
     singleChoice: '单选题',
     multiChoice: '多选题',
